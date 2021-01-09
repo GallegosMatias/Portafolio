78c3e4f699ab61df2ac1691a5e415f48eafdbae3
Irungaray <lionelirungaray@gmail.com>
2021-01-09 15:30:28 -0300

    Public Path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/', // Watch this
+    publicPath: './', // Watch this
   },
   resolve: {
     extensions: ['.js', '.jsx'],
